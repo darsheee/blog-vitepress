6a70ec5725323b863120fba1562b74c400e0e22f
honahec <honahec@gmail.com>
2024-11-03 00:02:37 +0800

    beta1

---
+++ .vitepress/config.mjs
@@ -0,0 +1,95 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "云朵角落",
+  description: "Honahec's Blog",
+  head: [
+    ["link", { rel: "icon", href: "/favicon.ico" }]
+  ],
+  outDir: "dist",
+  srcDir: "src",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+
+    logo: "/logo.png",
+
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: '示例', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: '示例',
+        items: [
+          { text: 'Markdown示例', link: '/markdown-examples' },
+          { text: 'Runtime API示例', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/Calm00' }
+    ],
+
+    search: {
+      provider: 'local',
+      options: {
+        miniSearch: {
+          options: {
+            
+          },
+          searchOptions: {
+           
+          }
+        },
+        translations: {
+          button: {
+            buttonText: '搜索',
+            buttonAriaLabel: '搜索'
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除搜索条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+              closeText: "关闭"
+            }
+          }
+        }
+      }
+    },
+
+    footer: {
+      message: `<a href="https://beian.miit.gov.cn/" target="_blank">鲁ICP备2024119517号-1</a>`,
+      copyright: `Copyright © 2024 Honahec`     
+    },
+
+    docFooter: {
+      prev: "上一页",
+      next: "下一页"
+    },
+
+    outline: {
+      level: [2, 6],
+      label: "文章目录"
+    },
+
+    lastUpdated: {
+      text: "最后更新于",
+      formatOptions: {
+        dateStyle: "short",
+        timeStyle: "short"
+      }
+    },
+
+    langMenuLabel: "选择语言",
+    returnToTopLabel: "返回顶部",
+    sidebarMenuLabel: "目录",
+    darkModeSwitchLabel: "主题",
+    lightModeSwitchTitle: "浅色模式",
+    darkModeSwitchTitle: "深色模式"
+  }
+})
---
+++ src/index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "云朵角落"
+  text: "Honahec's Blog"
+  tagline: 好记性不如烂笔头
+  actions:
+    - theme: brand
+      text: 第一个示例
+      link: /markdown-examples
+    - theme: alt
+      text: 第二个示例
+      link: /api-examples
+
+features:
+  - title: 特性一
+    details: 特性一的详细描述
+  - title: 特性二
+    details: 特性二的详细描述
+  - title: 特性三
+    details: 特性三的详细描述
+---
+
